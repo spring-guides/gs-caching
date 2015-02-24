40cae41ef12d0ae3347f21a77b5a03eb6355e207
Greg Turnquist <gturnquist@gopivotal.com>
2015-02-24 15:24:30 -0600

    Upgrade to @SpringBootApplication

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -2,19 +2,19 @@ package hello;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+@SpringBootApplication
 @EnableCaching
-@EnableAutoConfiguration
 public class Application {
 
     private static final Logger log = LoggerFactory.getLogger(Application.class);
--- initial/src/main/java/hello/Application.java
+++ initial/src/main/java/hello/Application.java
@@ -2,15 +2,15 @@ package hello;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@EnableAutoConfiguration
+@SpringBootApplication
 public class Application {
 
     private final static Logger log = LoggerFactory.getLogger(Application.class);
