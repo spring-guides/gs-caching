08710a50ac11dbfc50b9687c7c85d6eee4dabc53
Danil Pavlov <danil.pavlov@jetbrains.com>
2026-02-17 16:38:35 +0100

    fix: polishing

--- initial-kotlin/src/main/kotlin/com/example/caching/AppRunner.kt
+++ initial-kotlin/src/main/kotlin/com/example/caching/AppRunner.kt
@@ -7,12 +7,12 @@ import org.springframework.stereotype.Component
 @Component
 class AppRunner(private val bookRepository: BookRepository) : CommandLineRunner {
 
-    private val logger = LoggerFactory.getLogger(AppRunner::class.java)
+    private val logger = LoggerFactory.getLogger(javaClass)
 
     override fun run(vararg args: String) {
         logger.info(".... Fetching books")
-        logger.info("isbn-1234 -->" + bookRepository.getByIsbn("isbn-1234"))
-        logger.info("isbn-1234 -->" + bookRepository.getByIsbn("isbn-1234"))
-        logger.info("isbn-1234 -->" + bookRepository.getByIsbn("isbn-1234"))
+        logger.info("isbn-1234 -->${bookRepository.getByIsbn("isbn-1234")}")
+        logger.info("isbn-1234 -->${bookRepository.getByIsbn("isbn-1234")}")
+        logger.info("isbn-1234 -->${bookRepository.getByIsbn("isbn-1234")}")
     }
 }
